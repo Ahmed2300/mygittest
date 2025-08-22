11a1ea0f618f074950073444a2f7c9b90e5f6bf9
Ahmed-Amr <ahmedamraz@gmail.com>
2025-08-22 12:40:50 +0300

    lat update

--- app.js
+++ app.js
@@ -3,3 +3,4 @@
         const output = document.getElementById('outputField');
         output.textContent = `You entered: ${input.value}`;
     }
+    document.getElementById('submitButton').addEventListener('click', clickHandler);
